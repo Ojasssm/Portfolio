cf1267e68fa79a9975f3aebc18caafd886875540
Ojas Mishra <125395146+Ojasssm@users.noreply.github.com>
2023-04-09 21:36:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Running')
+
+let form = document.querySelector('.form')
+let workwithme = document.getElementsByClassName('workwithme')[0, 1]
+
+workwithme.addEventListener('click', ()=>{
+    console.log('clicked')
+})
+
+// let backward = document.querySelector('.serboxarrow1')
+// let forward = document.querySelector('.serboxarrow2')
+// let sbox = Array.from(document.querySelectorAll('.sbox'))
+
+// forward.addEventListener('click', ()=>{
+//     console.log('clicked')
+//     sbox.style.translate = '10px 0'
+// })
